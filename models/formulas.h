dfd3160410a47c0c7e9c472242aeb6f780907780
Alexey Min <alexey.min@gmail.com>
2017-12-25 16:49:44 +0300

    ehm.. missing formulas.h?

---
+++ models/formulas.h
@@ -0,0 +1,11 @@
+#ifndef H_FORMULAS
+#define H_FORMULAS
+
+namespace EM {
+
+float skill_points_per_minute(int attr_primary, int attr_secondary);
+int skill_points_needed_for_skill(int skill_rank, int skill_level);
+
+}
+
+#endif
