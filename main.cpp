0852c461df65a22cd4b420634685f4bb63531157
Alexey Min <alexey.min@gmail.com>
2018-02-26 12:53:16 +0300

    Bump Qt required version to 5.9, require QuickControls2 module
    experiment with QQuickStyle

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+#include <QGuiApplication>
+#include <QQuickStyle>
 #include "qmlevemon_app.h"
 
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    //QQuickStyle::setStyle(QLatin1String("Material"));
 
     EM::QmlEvemonApp app(argc, argv);
     if (!app.initQmlEngine()) {
