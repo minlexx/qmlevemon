5cbdd57974ccd8168c968aac83948bb83f00fd3f
Alexey Min <alexey.min@gmail.com>
2018-03-11 04:35:52 +0300

    Formulas: fix skill_points_needed_for_skill_level():
    - use round() on return value
    - return uint64_t

--- models/formulas.cpp
+++ models/formulas.cpp
@@ -13,19 +13,23 @@ float skill_points_per_minute(int attr_primary, int attr_secondary)
 {
     float fap = static_cast<float>(attr_primary);
     float fas = static_cast<float>(attr_secondary);
-    return fap + 0.5*fas;
+    return fap + 0.5f * fas;
 }
 
 
-int skill_points_needed_for_skill(int skill_rank, int skill_level)
+uint64_t skill_points_needed_for_skill_level(int skill_rank, int skill_level)
 {
-    // skillpoints = 2^(2.5(skilllevel−1))  * 250 * skillrank
-    Q_ASSERT(skill_rank >= 1);
-    Q_ASSERT(skill_level >= 1);
+    // only return valid result for valid params... no asserts in release code
+    if ((skill_rank <= 0) || (skill_level <= 0)) {
+        return 0;
+    }
+
+    // Formula is:
+    // skillpoints = 2^(2.5*(skilllevel−1))  * 250 * skillrank
     double stepen = 2.5 * (static_cast<double>(skill_level) - 1.0);
-    double res = pow(2, stepen);
+    double res = pow(2.0, stepen);
     res *= (250 * static_cast<double>(skill_rank));
-    return static_cast<int>(res);
+    return static_cast<uint64_t>(round(res));
 }
 
 }
--- models/formulas.h
+++ models/formulas.h
@@ -1,10 +1,12 @@
 #ifndef H_FORMULAS
 #define H_FORMULAS
 
+#include <stdint.h>
+
 namespace EM {
 
 float skill_points_per_minute(int attr_primary, int attr_secondary);
-int skill_points_needed_for_skill(int skill_rank, int skill_level);
+uint64_t skill_points_needed_for_skill_level(int skill_rank, int skill_level);
 
 }
 
