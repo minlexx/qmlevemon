b45167e35acb4992898e9c797fff84f46892aa8c
Alexey Min <alexey.min@gmail.com>
2018-03-19 02:47:00 +0300

    WIP

---
+++ models/character_skillqueue_model.h
@@ -0,0 +1,66 @@
+#ifndef H_EM_CHARACTER_SKILLQUEUE_MODEL
+#define H_EM_CHARACTER_SKILLQUEUE_MODEL
+
+#include <QObject>
+#include <QAbstractListModel>
+#include <QMutex>
+#include <QByteArray>
+#include <QVector>
+
+#include "character_skill.h"
+
+
+namespace EM {
+
+
+class CharacterSkillQueueModel: public QAbstractListModel
+{
+    Q_OBJECT
+
+    enum Roles {
+        // properties from skill template
+        SkillId = Qt::UserRole + 1,
+        SkillName,
+        SkillGroupName,
+        SkillGroupId,
+        PrimaryAttribute,
+        SecondaryAttribute,
+        SkillTimeConstant,
+        // properties from skill
+        TrainedLevel,
+        ActiveLevel,
+        TtrainingLevel,
+        TrainingLevelRoman,
+        SkillPointsInSkill,
+        SkillPointsInLevel,
+        // properties from skill queue
+        IsInQueue,
+        PositionInQueue,
+        TrainPercent,
+        TrainStartDate,
+        TrainFinishDate
+    };
+
+public:
+    explicit CharacterSkillQueueModel(QObject *parent = nullptr);
+
+public:  // reimplmented interface
+    QHash<int, QByteArray> roleNames() const override;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+public:
+    // other funcs
+    void addQueuedSkill(const CharacterSkill &skill);
+
+private:
+    QHash<int, QByteArray> m_roles;
+    QVector<CharacterSkill> m_data;
+    mutable QMutex m_mutex;
+};
+
+
+}
+
+
+#endif
