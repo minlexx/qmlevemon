d106bf1ae37b13906b2a1b0e76eb95880cdd13f1
Alexey Min <alexey.min@gmail.com>
2018-03-20 15:41:33 +0300

    WIP skeleton, to be able to compile

--- models/character_skillqueue_model.cpp
+++ models/character_skillqueue_model.cpp
@@ -1,1 +1,34 @@
 #include "character_skillqueue_model.h"
+
+EM::CharacterSkillQueueModel::CharacterSkillQueueModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    m_roles = {
+        {SkillId, QByteArrayLiteral("skillId")},
+        {SkillName, QByteArrayLiteral("skillName")},
+    };
+}
+
+QHash<int, QByteArray> EM::CharacterSkillQueueModel::roleNames() const
+{
+    return m_roles;
+}
+
+int EM::CharacterSkillQueueModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    QMutexLocker lock(&m_mutex);
+    return m_data.count();
+}
+
+QVariant EM::CharacterSkillQueueModel::data(const QModelIndex &index, int role) const
+{
+    Q_UNUSED(index)
+    Q_UNUSED(role)
+    return QVariant();
+}
+
+void EM::CharacterSkillQueueModel::addQueuedSkill(const EM::CharacterSkill &skill)
+{
+    Q_UNUSED(skill)
+}
