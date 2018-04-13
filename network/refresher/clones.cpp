c858f11e494dd934b6ce771fd41c42cbdf03f1cf
Alexey Min <alexey.min@gmail.com>
2018-04-13 17:47:51 +0300

    PeriodicalRefresher: some more boilerplate code for refreshing clones/ implants

--- network/refresher/clones.cpp
+++ network/refresher/clones.cpp
@@ -14,9 +14,60 @@ namespace EM {
 
 int PeriodicalRefresherWorker::resresh_clones(Character *ch)
 {
-    Q_UNUSED(ch)
+    if (!ch->updateTimestamps().isUpdateNeeded(UpdateTimestamps::UTST::CLONES)) {
+        qCDebug(logRefresher) << " no need to refresh clones for" << ch->toString();
+        return 0;  // no update needed, too early
+    }
+    // check if tokens needs refresing
+    if (!this->check_refresh_token(ch)) {
+        return 0;
+    }
+
+    qCDebug(logRefresher) << " refreshing clones for" << ch->toString();
+    QJsonObject reply;
+
+    // 1. refresh clones
+    if (m_api->get_character_clones(reply, ch->characterId(), ch->getAuthTokens().access_token)) {
+        qCDebug(logRefresher) << reply;
+        // example response:
+        //      {
+        //        "jump_clones": [
+        //          {
+        //            "jump_clone_id": 37293234,
+        //            "location_id": 60003466,
+        //            "location_type": "station",
+        //            "implants": [
+        //              10212,
+        //              10216,
+        //              13258,
+        //              13259,
+        //              19684
+        //            ],
+        //            "name": "Active armor"
+        //          },
+        //          {
+        //            "jump_clone_id": 36981517,
+        //            "location_id": 1021551599501,
+        //            "location_type": "structure",
+        //            "implants": [],
+        //            "name": ""
+        //          }
+        //        ],
+        //        "last_clone_jump_date": "2018-03-08T18:43:04Z",
+        //        "home_location": {
+        //          "location_id": 60003916,
+        //          "location_type": "station"
+        //        }
+        //      }
+    }
+
     // if (QThread::currentThread()->isInterruptionRequested()) break; // break early
-    return 0;
+
+    // 2, refresh implants
+    // example response : [ 22107, 22108, 22111, 22109, 22110, 13229, 13249 ]
+
+    ch->setUpdateTimestamp(UpdateTimestamps::UTST::LOCATION);
+    return 1; // 1 - there was an update
 }
 
 
