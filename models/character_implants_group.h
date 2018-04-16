9ce128d2a5ad4b30c7d2dc1cca281f4001ad123a
Alexey Min <alexey.min@gmail.com>
2018-04-16 14:01:01 +0300

    Models: CharacterImplantsGroup: add basic operators

--- models/character_implants_group.h
+++ models/character_implants_group.h
@@ -3,7 +3,16 @@
 
 #include <QObject>
 #include <QVector>
+#include <QDataStream>
+#include <QDebug>
 #include "invtype.h"
+
+
+namespace EM { class CharacterImplantsGroup; }
+
+QDataStream& operator<<(QDataStream &stream, const EM::CharacterImplantsGroup &impGroup);
+QDataStream& operator>>(QDataStream &stream, EM::CharacterImplantsGroup &impGroup);
+QDebug operator<<(QDebug stream, const EM::CharacterImplantsGroup &impGroup);
 
 
 namespace EM {
@@ -13,13 +22,22 @@ class CharacterImplantsGroup : public QObject
 {
     Q_OBJECT
 public:
-    explicit CharacterImplantsGroup(QObject *parent = nullptr);
+    CharacterImplantsGroup(QObject *parent = nullptr);
+    CharacterImplantsGroup(const CharacterImplantsGroup &other);
+    CharacterImplantsGroup(CharacterImplantsGroup &&other);
+    CharacterImplantsGroup &operator=(const CharacterImplantsGroup &other);
+    CharacterImplantsGroup &operator=(CharacterImplantsGroup &&other);
 
 private:
     QVector<InvType> m_implants;
+
+    friend QDataStream& (::operator<<)(QDataStream &stream, const CharacterImplantsGroup &impGroup);
+    friend QDataStream& (::operator>>)(QDataStream &stream, CharacterImplantsGroup &impGroup);
+    friend QDebug (::operator<<)(QDebug stream, const EM::CharacterImplantsGroup &impGroup);
 };
 
 
 } // namespace
+
 
 #endif // CHARACTERIMPLANTSGROUP_H
