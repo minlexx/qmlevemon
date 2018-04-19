6251cb375da07adbffc2f8b3d52460aa89a25b2a
Alexey Min <alexey.min@gmail.com>
2018-04-19 15:53:49 +0300

    AppSettings: interface for QML

--- settings.cpp
+++ settings.cpp
@@ -29,6 +29,31 @@ QNetworkProxy EM::AppSettings::proxySettings() const
     return QNetworkProxy(ptype, hostName, port, user, password);
 }
 
+int EM::AppSettings::proxyType() const
+{
+    return value(QLatin1String("proxyType"), 0).toInt();
+}
+
+QString EM::AppSettings::proxyHost() const
+{
+    return value(QLatin1String("proxyHost")).toString();
+}
+
+int EM::AppSettings::proxyPort() const
+{
+    return value(QLatin1String("proxyPort"), 0).toInt();
+}
+
+QString EM::AppSettings::proxyUser() const
+{
+    return value(QLatin1String("proxyUser")).toString();
+}
+
+QString EM::AppSettings::proxyPassword() const
+{
+    return value(QLatin1String("proxyPassword")).toString();
+}
+
 void EM::AppSettings::setProxySettings(const QNetworkProxy &p)
 {
     setValue(QLatin1String("proxyType"), static_cast<int>(p.type()));
@@ -36,4 +61,35 @@ void EM::AppSettings::setProxySettings(const QNetworkProxy &p)
     setValue(QLatin1String("proxyPort"), static_cast<int>(p.port()));
     setValue(QLatin1String("proxyUser"), p.user());
     setValue(QLatin1String("proxyPassword"), p.password());
+    Q_EMIT proxyConfigChanged();
+}
+
+void EM::AppSettings::setProxyType(int typ)
+{
+    setValue(QLatin1String("proxyType"), typ);
+    Q_EMIT proxyConfigChanged();
+}
+
+void EM::AppSettings::setProxyHost(const QString &host)
+{
+    setValue(QLatin1String("proxyHost"), host);
+    Q_EMIT proxyConfigChanged();
+}
+
+void EM::AppSettings::setProxyPort(int p)
+{
+    setValue(QLatin1String("proxyPort"), p);
+    Q_EMIT proxyConfigChanged();
+}
+
+void EM::AppSettings::setProxyUser(const QString &user)
+{
+    setValue(QLatin1String("proxyUser"), user);
+    Q_EMIT proxyConfigChanged();
+}
+
+void EM::AppSettings::setProxyPassword(const QString &password)
+{
+    setValue(QLatin1String("proxyPassword"), password);
+    Q_EMIT proxyConfigChanged();
 }
--- settings.h
+++ settings.h
@@ -9,15 +9,37 @@ namespace EM {
 
 class AppSettings: public QSettings
 {
+    Q_OBJECT
+    Q_PROPERTY(bool     isProxyEnabled READ isProxyEnabled WRITE setProxyEnabled  NOTIFY proxyConfigChanged)
+    Q_PROPERTY(int      proxyType      READ proxyType      WRITE setProxyType     NOTIFY proxyConfigChanged)
+    Q_PROPERTY(QString  proxyHost      READ proxyHost      WRITE setProxyHost     NOTIFY proxyConfigChanged)
+    Q_PROPERTY(int      proxyPort      READ proxyPort      WRITE setProxyPort     NOTIFY proxyConfigChanged)
+    Q_PROPERTY(QString  proxyUser      READ proxyUser      WRITE setProxyUser     NOTIFY proxyConfigChanged)
+    Q_PROPERTY(QString  proxyPassword  READ proxyPassword  WRITE setProxyPassword NOTIFY proxyConfigChanged)
+
 public:
     explicit AppSettings(QObject *parent = nullptr);
 
 public:
-    bool isProxyEnabled() const;
-    void setProxyEnabled(bool b);
-
+    bool          isProxyEnabled() const;
     QNetworkProxy proxySettings() const;
+    int           proxyType() const;
+    QString       proxyHost() const;
+    int           proxyPort() const;
+    QString       proxyUser() const;
+    QString       proxyPassword() const;
+
+public Q_SLOTS:
+    void setProxyEnabled(bool b);
     void setProxySettings(const QNetworkProxy &p);
+    void setProxyType(int typ);
+    void setProxyHost(const QString &host);
+    void setProxyPort(int p);
+    void setProxyUser(const QString &user);
+    void setProxyPassword(const QString &password);
+
+Q_SIGNALS:
+    void proxyConfigChanged();
 };
 
 
