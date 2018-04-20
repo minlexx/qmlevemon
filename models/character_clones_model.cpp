fdce6e863020b698e4f47452a539cc42006a24f2
Alexey Min <alexey.min@gmail.com>
2018-04-20 18:38:51 +0300

    First implementation if CharacterClonesModel

--- models/character_clones_model.cpp
+++ models/character_clones_model.cpp
@@ -1,6 +1,74 @@
 #include "character_clones_model.h"
 
-CharacterClonesModel::CharacterClonesModel(QObject *parent) : QObject(parent)
-{
 
+namespace EM {
+
+
+CharacterClonesModel::CharacterClonesModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    m_roles = {
+        {Roles::CloneId,            QByteArrayLiteral("cloneId")},
+        {Roles::CloneName,          QByteArrayLiteral("cloneName")},
+        {Roles::CloneLocation,      QByteArrayLiteral("cloneLocation")},
+        {Roles::CloneImplantsModel, QByteArrayLiteral("cloneImplantsModel")},
+    };
 }
+
+QHash<int, QByteArray> CharacterClonesModel::roleNames() const
+{
+    return m_roles;
+}
+
+int CharacterClonesModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return m_clones.count();
+}
+
+QVariant CharacterClonesModel::data(const QModelIndex &index, int role) const
+{
+    QVariant ret;
+    if (!index.isValid()) {
+        return ret;
+    }
+    const int row = index.row();
+    if ((row < 0) || (row >= m_clones.size())) {
+        return ret;
+    }
+    const CharacterClone &clone = m_clones.at(row);
+    switch (role) {
+    case Roles::CloneId:
+        ret = clone.cloneId();
+        break;
+    case Roles::CloneName:
+        ret = clone.cloneName();
+        break;
+    case Roles::CloneLocation:
+        ret = clone.locationObj();
+        break;
+    case Roles::CloneImplantsModel:
+        ret = clone.implantsModel();
+        break;
+    }
+    return ret;
+}
+
+QVector<CharacterClone> &CharacterClonesModel::clones()
+{
+    return m_clones;
+}
+
+const QVector<CharacterClone> &CharacterClonesModel::clones() const
+{
+    return m_clones;
+}
+
+void CharacterClonesModel::setClones(const QVector<CharacterClone> &clns)
+{
+    // TODO: implement CharacterClonesModel::setClones()
+    Q_UNUSED(clns)
+}
+
+
+} // namespace EM
--- models/character_clones_model.h
+++ models/character_clones_model.h
@@ -2,16 +2,46 @@
 #define CHARACTER_CLONES_MODEL_H
 
 #include <QObject>
+#include <QAbstractListModel>
+#include <QVector>
+#include <QHash>
+#include <QByteArray>
+#include "character_clone.h"
 
-class CharacterClonesModel : public QObject
+
+namespace EM {
+
+
+class CharacterClonesModel : public QAbstractListModel
 {
     Q_OBJECT
+
+    enum Roles {
+        CloneName,
+        CloneId,
+        CloneLocation,
+        CloneImplantsModel
+    };
+
 public:
     explicit CharacterClonesModel(QObject *parent = nullptr);
 
-signals:
+public:  // reimplmented interface
+    QHash<int, QByteArray> roleNames() const override;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
-public slots:
+public:
+    QVector<CharacterClone> &clones();
+    const QVector<CharacterClone> &clones() const;
+    void setClones(const QVector<CharacterClone> &clns);
+
+private:
+    QHash<int, QByteArray>  m_roles;
+    QVector<CharacterClone> m_clones;
 };
+
+
+} // namespace
 
 #endif // CHARACTER_CLONES_MODEL_H
