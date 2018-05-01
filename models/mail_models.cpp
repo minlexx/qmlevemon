4dc271842c7dbeb75b98c0e8b6729542c8781ed3
Alexey Min <alexey.min@gmail.com>
2018-05-01 21:50:00 +0300

    Models: first work on mail models

--- models/mail_models.cpp
+++ models/mail_models.cpp
@@ -1,9 +1,88 @@
+#include <QDataStream>
 #include "mail_models.h"
+
 
 namespace EM {
 
 
-//
+MailRecipient::Type MailRecipient::typeFromString(const QString &typeName)
+{
+    Type ret = Type::None;
+    if (typeName == QLatin1String("character")) {
+        ret = Character;
+    } else if (typeName == QLatin1String("corporation")) {
+        ret = Corporation;
+    } else if (typeName == QLatin1String("alliance")) {
+        ret = Alliance;
+    } else if (typeName == QLatin1String("mailing_list")) {
+        ret = MailingList;
+    }
+    return ret;
+}
 
 
-} // namespace EM
+}  // namespace EM
+
+
+QDataStream &operator<<(QDataStream &stream, const EM::MailLabel &label)
+{
+    stream << label.id;
+    stream << label.name;
+    stream << label.color;
+    stream << label.unread_count;
+    return stream;
+}
+
+QDataStream &operator>>(QDataStream &stream, EM::MailLabel &label)
+{
+    stream >> label.id;
+    stream >> label.name;
+    stream >> label.color;
+    stream >> label.unread_count;
+    return stream;
+}
+
+QDataStream &operator<<(QDataStream &stream, const EM::MailRecipient &recipient)
+{
+    stream << recipient.id;
+    stream << recipient.name;
+    stream << static_cast<int>(recipient.type);
+    return stream;
+}
+
+
+QDataStream &operator>>(QDataStream &stream, EM::MailRecipient &recipient)
+{
+    int i = 0;
+    stream >> recipient.id;
+    stream >> recipient.name;
+    stream >> i;
+    recipient.type = static_cast<EM::MailRecipient::Type>(i);
+    return stream;
+}
+
+QDataStream &operator<<(QDataStream &stream, const EM::Mail &mail)
+{
+    stream << mail.id;
+    stream << mail.body;
+    stream << mail.subject;
+    stream << mail.id_from;
+    stream << mail.is_read;
+    stream << mail.timestamp;
+    stream << mail.labels;
+    stream << mail.recipients;
+    return stream;
+}
+
+QDataStream &operator>>(QDataStream &stream, EM::Mail &mail)
+{
+    stream >> mail.id;
+    stream >> mail.body;
+    stream >> mail.subject;
+    stream >> mail.id_from;
+    stream >> mail.is_read;
+    stream >> mail.timestamp;
+    stream >> mail.labels;
+    stream >> mail.recipients;
+    return stream;
+}
--- models/mail_models.h
+++ models/mail_models.h
@@ -2,25 +2,87 @@
 #define MAIL_MODELS_H
 
 #include <QObject>
+#include <QColor>
+#include <QVector>
+#include <QDateTime>
+
+
+QT_FORWARD_DECLARE_CLASS(QDataStream)
+
 
 namespace EM {
 
 
 class MailLabel {
-    //
+public:
+    MailLabel() {}
+    MailLabel(const MailLabel &) = default;
+    MailLabel(MailLabel &&) = default;
+    MailLabel &operator=(const MailLabel &) = default;
+    MailLabel &operator=(MailLabel &&) = default;
+
+    quint64 id = 0;
+    QString name;
+    QColor color;
+    int unread_count = 0;
 };
 
 
 class MailRecipient {
-    //
+public:
+    enum Type {
+        None,
+        Character,
+        Corporation,
+        Alliance,
+        MailingList
+    };
+
+    MailRecipient() { }
+    MailRecipient(const MailRecipient &) = default;
+    MailRecipient(MailRecipient &&) = default;
+    MailRecipient& operator=(const MailRecipient &) = default;
+    MailRecipient& operator=(MailRecipient &&) = default;
+
+    static Type typeFromString(const QString &typeName);
+
+    quint64 id = 0;
+    Type type = Type::None;
+    QString name;
 };
 
 
 class Mail {
-    //
+public:
+    Mail() { }
+    Mail(const Mail &) = default;
+    Mail(Mail &&) = default;
+    Mail &operator=(const Mail &) = default;
+    Mail &operator=(Mail &&) = default;
+
+public:
+    quint64 id = 0;
+    QString body;
+    QString subject;
+    quint64 id_from = 0;
+    bool is_read = false;
+    QDateTime timestamp;
+    QVector<quint64> labels;
+    QVector<MailRecipient> recipients;
 };
 
 
 } // namespace
+
+
+QDataStream &operator<<(QDataStream &stream, const EM::MailLabel &label);
+QDataStream &operator>>(QDataStream &stream, EM::MailLabel &label);
+
+QDataStream &operator<<(QDataStream &stream, const EM::MailRecipient &recipient);
+QDataStream &operator>>(QDataStream &stream, EM::MailRecipient &recipient);
+
+QDataStream &operator<<(QDataStream &stream, const EM::Mail &mail);
+QDataStream &operator>>(QDataStream &stream, EM::Mail &mail);
+
 
 #endif // MAIL_MODELS_H
