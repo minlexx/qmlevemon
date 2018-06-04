cc012866247e4958b4ce58ae3b3a86ef0ee012b1
Alexey Min <alexey.min@gmail.com>
2018-06-04 12:01:33 +0300

    models: very basic implementation of Notification object

--- models/notification.cpp
+++ models/notification.cpp
@@ -1,2 +1,20 @@
 #include "notification.h"
 
+
+bool Notification::operator==(const Notification &o) const
+{
+    if ((notificationId == o.notificationId)
+            && (notificationType == o.notificationType)
+            && (senderId == o.senderId)
+            && (senderType == o.senderType)
+            && (timestamp == o.timestamp)
+            && (text == o.text)) {
+        return true;
+    }
+    return false;
+}
+
+bool Notification::operator!=(const Notification &o) const
+{
+    return !((*this) == o);
+}
--- models/notification.h
+++ models/notification.h
@@ -1,11 +1,27 @@
 #ifndef NOTIFICATION_H
 #define NOTIFICATION_H
 
+#include <QString>
+#include <QDateTime>
 
 class Notification
 {
 public:
     Notification() = default;
+    Notification(const Notification &) = default;
+    Notification(Notification &&) = default;
+    Notification &operator=(const Notification &) = default;
+    Notification &operator=(Notification &&) = default;
+    bool operator==(const Notification &o) const;
+    bool operator!=(const Notification &o) const;
+
+public:
+    quint64 notificationId = 0;
+    QString notificationType;
+    quint64 senderId = 0;
+    QString senderType;
+    QDateTime timestamp;
+    QString text;
 };
 
 #endif // NOTIFICATION_H
