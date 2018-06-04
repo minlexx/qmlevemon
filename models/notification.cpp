b3014d8e357c1a1a6f7a4b22ff366ff586aee2c3
Alexey Min <alexey.min@gmail.com>
2018-06-04 13:10:10 +0300

    Let there be model and serialization operators

--- models/notification.cpp
+++ models/notification.cpp
@@ -1,4 +1,8 @@
+#include <QDataStream>
 #include "notification.h"
+
+
+namespace EM {
 
 
 bool Notification::operator==(const Notification &o) const
@@ -18,3 +22,153 @@ bool Notification::operator!=(const Notification &o) const
 {
     return !((*this) == o);
 }
+
+
+EM::CharacterNotifications::CharacterNotifications(QObject *parent)
+    : QAbstractListModel(parent)
+{
+}
+
+CharacterNotifications::CharacterNotifications(const CharacterNotifications &other)
+    : QAbstractListModel(other.parent())
+{
+    (*this) = other;
+}
+
+CharacterNotifications::CharacterNotifications(CharacterNotifications &&other)
+    : QAbstractListModel(other.parent())
+{
+    (*this) = std::move(other);
+}
+
+CharacterNotifications &CharacterNotifications::operator=(const CharacterNotifications &other)
+{
+    if (this == &other) return (*this);
+    beginResetModel();
+    m_data = other.m_data;
+    endResetModel();
+    return (*this);
+}
+
+CharacterNotifications &CharacterNotifications::operator=(CharacterNotifications &&other)
+{
+    if (this == &other) return (*this);
+    beginResetModel();
+    m_data = std::move(other.m_data);
+    endResetModel();
+    return (*this);
+}
+
+bool CharacterNotifications::operator==(const CharacterNotifications &other) const
+{
+    return m_data == other.m_data;
+}
+
+bool CharacterNotifications::operator!=(const CharacterNotifications &other) const
+{
+    return !((*this) == other);
+}
+
+int CharacterNotifications::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return m_data.size();
+}
+
+QHash<int, QByteArray> CharacterNotifications::roleNames() const
+{
+    static QHash<int,QByteArray> roles = {
+        {Qt::DisplayRole,    QByteArrayLiteral("type")},
+        {Roles::Id,          QByteArrayLiteral("id")},
+        {Roles::Type,        QByteArrayLiteral("type")},
+        {Roles::SenderId,    QByteArrayLiteral("senderId")},
+        {Roles::SenderType,  QByteArrayLiteral("senderType")},
+        {Roles::Timestamp,   QByteArrayLiteral("timestamp")},
+        {Roles::Text,        QByteArrayLiteral("text")},
+    };
+    return roles;
+}
+
+QVariant CharacterNotifications::data(const QModelIndex &index, int role) const
+{
+    QVariant ret;
+    if (!index.isValid()) {
+        return ret;
+    }
+    int row = index.row();
+    if ((row < 0) || (row >= m_data.size())) {
+        return ret;
+    }
+    const Notification &ntf = m_data.at(row);
+    switch (role) {
+    case Qt::DisplayRole:
+    case Roles::Type:
+        ret = ntf.notificationType;
+        break;
+    case Roles::Id:
+        ret = ntf.notificationId;
+        break;
+    case Roles::SenderId:
+        ret = ntf.senderId;
+        break;
+    case Roles::SenderType:
+        ret = ntf.senderType;
+        break;
+    case Roles::Timestamp:
+        ret = ntf.timestamp;
+        break;
+    case Roles::Text:
+        ret = ntf.text;
+        break;
+    }
+    return ret;
+}
+
+QVector<Notification> &CharacterNotifications::internalData()
+{
+    return m_data;
+}
+
+const QVector<Notification> &CharacterNotifications::internalData() const
+{
+    return m_data;
+}
+
+
+} // namespace EM
+
+
+QDataStream &operator<<(QDataStream &stream, const EM::Notification &ntf)
+{
+    stream << ntf.notificationId;
+    stream << ntf.notificationType;
+    stream << ntf.senderId;
+    stream << ntf.senderType;
+    stream << ntf.timestamp;
+    stream << ntf.text;
+    return stream;
+}
+
+QDataStream &operator>>(QDataStream &stream, EM::Notification &ntf)
+{
+    stream >> ntf.notificationId;
+    stream >> ntf.notificationType;
+    stream >> ntf.senderId;
+    stream >> ntf.senderType;
+    stream >> ntf.timestamp;
+    stream >> ntf.text;
+    return stream;
+}
+
+QDataStream &operator<<(QDataStream &stream, const EM::CharacterNotifications &charNotifications)
+{
+    stream << charNotifications.m_data;
+    return stream;
+}
+
+QDataStream &operator>>(QDataStream &stream, EM::CharacterNotifications &charNotifications)
+{
+    stream >> charNotifications.m_data;
+    return stream;
+}
+
--- models/notification.h
+++ models/notification.h
@@ -1,8 +1,32 @@
 #ifndef NOTIFICATION_H
 #define NOTIFICATION_H
 
+#include <QByteArray>
 #include <QString>
 #include <QDateTime>
+#include <QObject>
+#include <QVector>
+#include <QHash>
+#include <QAbstractListModel>
+
+QT_FORWARD_DECLARE_CLASS(QDataStream)
+
+// forward declare data stream operators
+
+namespace EM {
+    class Notification;
+    class CharacterNotifications;
+}
+
+
+QDataStream& operator<<(QDataStream &stream, const EM::Notification &ntf);
+QDataStream& operator>>(QDataStream &stream, EM::Notification &ntf);
+
+QDataStream& operator<<(QDataStream &stream, const EM::CharacterNotifications &charMailLabels);
+QDataStream& operator>>(QDataStream &stream, EM::CharacterNotifications &charMailLabels);
+
+
+namespace EM {
 
 class Notification
 {
@@ -23,5 +47,50 @@ public:
     QDateTime timestamp;
     QString text;
 };
+
+
+class CharacterNotifications: public QAbstractListModel
+{
+    Q_OBJECT
+private:
+    enum Roles {
+        Id = Qt::UserRole + 1, Type, SenderId, SenderType, Timestamp, Text
+    };
+
+public:
+    CharacterNotifications(QObject *parent = nullptr);
+    CharacterNotifications(const CharacterNotifications &other);
+    CharacterNotifications(CharacterNotifications &&other);
+    CharacterNotifications &operator=(const CharacterNotifications &other);
+    CharacterNotifications &operator=(CharacterNotifications &&other);
+    bool operator==(const CharacterNotifications &other) const;
+    bool operator!=(const CharacterNotifications &other) const;
+
+public:
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QHash<int,QByteArray> roleNames() const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+public: // for internal use, m_data getter
+    QVector<Notification> &internalData();
+    const QVector<Notification> &internalData() const;
+
+protected:
+    QVector<Notification> m_data;
+
+    friend QDataStream& (::operator<<)(QDataStream &stream, const EM::CharacterNotifications &charNotifications);
+    friend QDataStream& (::operator>>)(QDataStream &stream, EM::CharacterNotifications &charNotifications);
+};
+
+
+} // namespace EM
+
+
+QDataStream& operator<<(QDataStream &stream, const EM::Notification &ntf);
+QDataStream& operator>>(QDataStream &stream, EM::Notification &ntf);
+
+QDataStream& operator<<(QDataStream &stream, const EM::CharacterNotifications &charNotifications);
+QDataStream& operator>>(QDataStream &stream, EM::CharacterNotifications &charNotifications);
+
 
 #endif // NOTIFICATION_H
