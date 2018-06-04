7b5e36032e079176e753eadc19fa3ad3e71d9668
Alexey Min <alexey.min@gmail.com>
2018-06-04 17:47:10 +0300

    Actually call refresh_notifications()

--- network/refresher/periodical_refresher_worker.cpp
+++ network/refresher/periodical_refresher_worker.cpp
@@ -75,6 +75,10 @@ void PeriodicalRefresherWorker::refresh()
         num_updates += this->refresh_mail(ch);
         if (QThread::currentThread()->isInterruptionRequested()) break; // break early
 
+        // notifications
+        num_updates += this->refresh_notifications(ch);
+        if (QThread::currentThread()->isInterruptionRequested()) break; // break early
+
         // in the case of any updates were made to this character,
         // after all data was updated, return modified copy to model
         if (num_updates > 0) {
