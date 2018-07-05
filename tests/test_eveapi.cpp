e252929306821c6099f5bc3efb51db1f252edeba
Alexey Min <alexey.min@gmail.com>
2018-07-05 19:09:49 +0300

    WIP: Try to write ESI API test

--- tests/test_eveapi.cpp
+++ tests/test_eveapi.cpp
@@ -1,8 +1,15 @@
 #include <QObject>
 #include <QVector>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QJsonObject>
 #include <QtTest>
+#include <QDebug>
+#include <QLoggingCategory>
 
 #include "eve_api/eve_api.h"
+
+Q_LOGGING_CATEGORY(logTestApi, "test.eveapi")
 
 class TestEveApi: public QObject
 {
@@ -11,15 +18,31 @@ class TestEveApi: public QObject
 private:
     QDateTime dtCur;
     EM::EveApi m_api;
+    QVector<quint64> ids_vector;
 
 private Q_SLOTS:
 
-    void initTestCase() { }
+    void initTestCase() {
+        ids_vector << 91205062; // Lexx Min
+        ids_vector << 95367553; // Alya Kastra
+    }
 
     void cleanupTestCase() { }
 
+    void test_characters_names() {
+        QJsonArray replyArr;
+        qCDebug(logTestApi) << "calling get_characters_names()";
+        bool api_ret = m_api.get_characters_names(replyArr, ids_vector);
+        qCDebug(logTestApi) << " get_characters_names() = " << replyArr;
+        QVERIFY2(!api_ret, "Api call get_characters_names() should fail");
+    }
+
     void test_universe_names() {
-        //QVERIFY(qi2.startDate == qi1.startDate);
+        QJsonArray replyArr;
+        qCDebug(logTestApi) << "calling post_universe_names()";
+        bool ret = m_api.post_universe_names(replyArr, ids_vector);
+        qCDebug(logTestApi) << " post_universe_names() = " << replyArr;
+        QVERIFY2(ret, "Api call post_universe_names() should be OK");
     }
 };
 
