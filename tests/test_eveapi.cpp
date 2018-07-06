14d723c7838b07aac7e43f9dcf5336080029166d
Alexey Min <alexey.min@gmail.com>
2018-07-06 13:46:00 +0300

    Tests/eveapi: complete writing test

--- tests/test_eveapi.cpp
+++ tests/test_eveapi.cpp
@@ -6,6 +6,8 @@
 #include <QtTest>
 #include <QDebug>
 #include <QLoggingCategory>
+//#include <QNetworkProxy>
+//#include <QNetworkProxyFactory>
 
 #include "eve_api/eve_api.h"
 
@@ -23,8 +25,13 @@ private:
 private Q_SLOTS:
 
     void initTestCase() {
+        // vector of IDs
         ids_vector << 91205062; // Lexx Min
         ids_vector << 95367553; // Alya Kastra
+
+        // app global proxy
+        //QNetworkProxyFactory *f = m_api.nam()->proxyFactory();
+        //qCDebug(logTestApi) << "API's proxy factory:" << f;
     }
 
     void cleanupTestCase() { }
@@ -33,16 +40,19 @@ private Q_SLOTS:
         QJsonArray replyArr;
         qCDebug(logTestApi) << "calling get_characters_names()";
         bool api_ret = m_api.get_characters_names(replyArr, ids_vector);
-        qCDebug(logTestApi) << " get_characters_names() = " << replyArr;
+        qCDebug(logTestApi) << " get_characters_names() = " << api_ret << replyArr;
+
         QVERIFY2(!api_ret, "Api call get_characters_names() should fail");
     }
 
     void test_universe_names() {
         QJsonArray replyArr;
         qCDebug(logTestApi) << "calling post_universe_names()";
-        bool ret = m_api.post_universe_names(replyArr, ids_vector);
-        qCDebug(logTestApi) << " post_universe_names() = " << replyArr;
-        QVERIFY2(ret, "Api call post_universe_names() should be OK");
+        bool api_ret = m_api.post_universe_names(replyArr, ids_vector);
+        qCDebug(logTestApi) << " post_universe_names() = " << api_ret << replyArr;
+
+        QVERIFY2(api_ret, "Api call post_universe_names() should be OK");
+        QVERIFY(replyArr.size() == 2);
     }
 };
 
