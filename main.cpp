1cb123da68351dfebd345dd20cf033a6952cd28d
Alexey Min <alexey.min@gmail.com>
2018-07-17 17:53:51 +0300

    Force breeze icon theme and default style under flatpak

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
 #include <QQuickStyle>
 #include "qmlevemon_app.h"
 
+#if defined(FLATPAK_BUILD)
+#include <QIcon>
+#endif
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    //QQuickStyle::setStyle(QLatin1String("Material"));
+    // QQuickStyle::setStyle(QLatin1String("Material"));
+
+#if defined(FLATPAK_BUILD)
+    QIcon::setThemeName(QStringLiteral("breeze"));
+    QQuickStyle::setStyle(QLatin1String("Default"));
+#endif
 
     EM::QmlEvemonApp app(argc, argv);
     if (!app.initQmlEngine()) {
