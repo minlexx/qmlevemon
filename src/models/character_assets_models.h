a5c9efcf3b942055f096b21739236167e667fa80
Alexey Min <alexey.min@gmail.com>
2018-08-01 13:52:48 +0300

    WIP

--- src/models/character_assets_models.h
+++ src/models/character_assets_models.h
@@ -3,6 +3,25 @@
 
 #include <QObject>
 
+
+class AssetEntry {
+public:
+    AssetEntry() = default;
+    AssetEntry(const AssetEntry &) = default;
+    AssetEntry(AssetEntry &&) = default;
+    ~AssetEntry() = default;
+    AssetEntry &operator=(const AssetEntry &) = default;
+    AssetEntry &operator=(AssetEntry &&) = default;
+
+    quint64 item_id = 0;
+    quint64 type_id = 0;
+    QString type_name;
+    bool is_singleton = false;
+    quint64 quantity = 0;
+    quint64 location_id = 0;
+};
+
+
 //class CharacterAssetsModels: public QObject
 //{
 //    Q_OBJECT
