a95f06c6d974d585f96229e612e8c0083cbcf1f8
Alexey Min <alexey.min@gmail.com>
2018-08-02 12:59:32 +0300

    Refresher/assets: try to fetch all items character owns, all pages

--- src/network/refresher/assets.cpp
+++ src/network/refresher/assets.cpp
@@ -18,7 +18,7 @@ int PeriodicalRefresherWorker::refresh_assets(Character *ch)
     int num_changes = 0;
 
     if (!ch->updateTimestamps().isUpdateNeeded(UpdateTimestamps::UTST::ASSETS)) {
-        qCDebug(logRefresher) << " no need to refresh wallet history for" << ch->toString();
+        qCDebug(logRefresher) << " no need to refresh assets for" << ch->toString();
         return num_changes;  // no update needed, too early
     }
     // check if tokens needs refresing
@@ -31,12 +31,28 @@ int PeriodicalRefresherWorker::refresh_assets(Character *ch)
 
     QJsonArray replyArr;
     qCDebug(logRefresher) << " refreshing assets for" << ch->toString();
-    if (m_api->get_character_assets(replyArr, ch->characterId(), ch->accessToken())) {
+    int page = 0, total_pages = 0;
+    if (m_api->get_character_assets(replyArr, ch->characterId(), ch->accessToken(), page, &total_pages)) {
         for (const QJsonValue &jval: replyArr) {
             const QJsonObject jobj = jval.toObject();
             AssetEntry entry = AssetEntry::fromJson(jobj);
             assetsModel.internalData().push_back(std::move(entry));
         }
+        // request extra pages if any
+        if (total_pages > 1) {
+            // first page was already requested
+            for (page = 2; page <= total_pages; page++) {
+                qCDebug(logRefresher) << " refreshing assets page" << page << "for" << ch->toString();
+                if (m_api->get_character_assets(replyArr, ch->characterId(), ch->accessToken(), page)) {
+                    for (const QJsonValue &jval: replyArr) {
+                        const QJsonObject jobj = jval.toObject();
+                        AssetEntry entry = AssetEntry::fromJson(jobj);
+                        assetsModel.internalData().push_back(std::move(entry));
+                    }
+                }
+            }
+        }
+        qCDebug(logRefresher) << "     total assets fetched:" << assetsModel.rowCount();
         ch->setAssetsModel(assetsModel);
         num_changes++;
     }
