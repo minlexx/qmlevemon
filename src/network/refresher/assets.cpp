3712e4e50022f5b5db9b2eafbefa1222c0b8d7b5
Alexey Min <alexey.min@gmail.com>
2018-08-02 11:56:11 +0300

    Refresher/assets: actually request first 1000 aseets

--- src/network/refresher/assets.cpp
+++ src/network/refresher/assets.cpp
@@ -17,6 +17,33 @@ int PeriodicalRefresherWorker::refresh_assets(Character *ch)
 {
     int num_changes = 0;
 
+    if (!ch->updateTimestamps().isUpdateNeeded(UpdateTimestamps::UTST::ASSETS)) {
+        qCDebug(logRefresher) << " no need to refresh wallet history for" << ch->toString();
+        return num_changes;  // no update needed, too early
+    }
+    // check if tokens needs refresing
+    if (!this->check_refresh_token(ch)) {
+        return num_changes;
+    }
+
+    CharacterAssetsModel assetsModel;
+    // Db *db = globalAppDatabaseInstance();
+
+    QJsonArray replyArr;
+    qCDebug(logRefresher) << " refreshing assets for" << ch->toString();
+    if (m_api->get_character_assets(replyArr, ch->characterId(), ch->accessToken())) {
+        for (const QJsonValue &jval: replyArr) {
+            const QJsonObject jobj = jval.toObject();
+            AssetEntry entry = AssetEntry::fromJson(jobj);
+            assetsModel.internalData().push_back(std::move(entry));
+        }
+        ch->setAssetsModel(assetsModel);
+        num_changes++;
+    }
+
+    if (num_changes > 0) {
+        ch->setUpdateTimestamp(UpdateTimestamps::UTST::ASSETS);
+    }
     return num_changes;
 }
 
