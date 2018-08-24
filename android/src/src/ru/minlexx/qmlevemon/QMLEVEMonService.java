ed1c0b4c8c4b3ec1677f93d42bb9241d5ef18b05
Alexey Min <alexey.min@gmail.com>
2018-08-24 19:16:29 +0300

    Android: add notify() methid to BG service

--- android/src/src/ru/minlexx/qmlevemon/QMLEVEMonService.java
+++ android/src/src/ru/minlexx/qmlevemon/QMLEVEMonService.java
@@ -1,5 +1,7 @@
 package ru.minlexx.qmlevemon;
  
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -18,5 +20,16 @@ public class QMLEVEMonService extends QtService
     public static void stopMyService(Context ctx) {
         Log.d(TAG, "Stopping service");
         ctx.stopService(new Intent(ctx, QMLEVEMonService.class));
+    }
+
+    public void notify(String title, String text)
+    {
+        Log.d(TAG, "notify()");
+        NotificationManager mgr = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setSmallIcon(R.drawable.icon);
+        builder.setContentTitle(title);
+        builder.setContentText(text);
+        mgr.notify(2, builder.build());
     }
 } 
